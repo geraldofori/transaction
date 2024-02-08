e6ddcc087654c360b3df8cd9fca7f7a86791d8fc
geraldofori <oforige@gmail.com>
2024-02-08 22:45:57 +0000

    added setter and getter method for the id property

--- src/main/java/com/example/transactions/entity/Transaction.java
+++ src/main/java/com/example/transactions/entity/Transaction.java
@@ -19,4 +19,12 @@ public class Transaction {
     private User receiver;
     private double amount;
     private Date transactionDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
