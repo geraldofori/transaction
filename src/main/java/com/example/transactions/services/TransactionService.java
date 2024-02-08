47d4d8fa2ecedb3f535af9a3b152b46faa3a59f8
geraldofori <oforige@gmail.com>
2024-02-08 22:54:31 +0000

    implemented exception on the crud methods

--- src/main/java/com/example/transactions/services/TransactionService.java
+++ src/main/java/com/example/transactions/services/TransactionService.java
@@ -1,6 +1,8 @@
 package com.example.transactions.services;
 
 import com.example.transactions.entity.Transaction;
+import com.example.transactions.exceptions.InvalidTransactionException;
+import com.example.transactions.exceptions.TransactionNotFoundException;
 import com.example.transactions.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,20 +23,28 @@ public class TransactionService {
 
     public Transaction getTransactionById(Long id) {
         Optional<Transaction> optionalTransaction = transactionRepository.findById(id);
-        return optionalTransaction.orElse(null);
+        return optionalTransaction.orElseThrow(()-> new TransactionNotFoundException("Transaction not found for id"+ id));
     }
 
     public Transaction createTransaction(Transaction transaction) {
+        if (transaction.getAmount() <= 0) {
+            throw new InvalidTransactionException("Invalid transaction amount");
+        }
+
         return transactionRepository.save(transaction);
     }
 
     public Transaction updateTransaction(Long id, Transaction updatedTransaction) {
-        if (transactionRepository.existsById(id)) {
-            updatedTransaction.setId(id);
-            return transactionRepository.save(updatedTransaction);
-        } else {
-            return null; // Handle case when the transaction with the given ID doesn't exist
+        if (!transactionRepository.existsById(id)) {
+            throw new TransactionNotFoundException("Transaction not found for id: " + id);
         }
+
+        if (updatedTransaction.getAmount() <= 0) {
+            throw new InvalidTransactionException("Invalid transaction amount");
+        }
+
+        updatedTransaction.setId(id);
+        return transactionRepository.save(updatedTransaction);
     }
 
     public void deleteTransaction(Long id) {
