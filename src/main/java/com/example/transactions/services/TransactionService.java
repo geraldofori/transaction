7697e48bedc7fb2080ada9b2f9daac5db373b278
geraldofori <oforige@gmail.com>
2024-02-08 22:46:40 +0000

    defined crud operations

--- src/main/java/com/example/transactions/services/TransactionService.java
+++ src/main/java/com/example/transactions/services/TransactionService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TransactionService {
@@ -14,6 +15,29 @@ public class TransactionService {
     TransactionRepository transactionRepository;
 
     public List<Transaction> getAllTransactions() {
+
         return transactionRepository.findAll();
+    }
+
+    public Transaction getTransactionById(Long id) {
+        Optional<Transaction> optionalTransaction = transactionRepository.findById(id);
+        return optionalTransaction.orElse(null);
+    }
+
+    public Transaction createTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    public Transaction updateTransaction(Long id, Transaction updatedTransaction) {
+        if (transactionRepository.existsById(id)) {
+            updatedTransaction.setId(id);
+            return transactionRepository.save(updatedTransaction);
+        } else {
+            return null; // Handle case when the transaction with the given ID doesn't exist
+        }
+    }
+
+    public void deleteTransaction(Long id) {
+        transactionRepository.deleteById(id);
     }
 }
