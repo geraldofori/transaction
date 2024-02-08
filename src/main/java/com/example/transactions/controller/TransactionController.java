e85115c634c165265cc0f3ea0fa5ed33efe63957
geraldofori <oforige@gmail.com>
2024-02-08 22:47:05 +0000

    implemented the crud operations

--- src/main/java/com/example/transactions/controller/TransactionController.java
+++ src/main/java/com/example/transactions/controller/TransactionController.java
@@ -3,9 +3,7 @@ package com.example.transactions.controller;
 import com.example.transactions.entity.Transaction;
 import com.example.transactions.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,27 @@ public class TransactionController {
 
     @GetMapping
     public List<Transaction> getAllTransactions() {
+
         return transactionService.getAllTransactions();
+    }
+
+    @GetMapping("/{id}")
+    public Transaction getTransactionById(@PathVariable Long id) {
+        return transactionService.getTransactionById(id);
+    }
+
+    @PostMapping
+    public Transaction createTransaction(@RequestBody Transaction transaction) {
+        return transactionService.createTransaction(transaction);
+    }
+
+    @PutMapping("/{id}")
+    public Transaction updateTransaction(@PathVariable Long id, @RequestBody Transaction updatedTransaction) {
+        return transactionService.updateTransaction(id, updatedTransaction);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTransaction(@PathVariable Long id) {
+        transactionService.deleteTransaction(id);
     }
 }
